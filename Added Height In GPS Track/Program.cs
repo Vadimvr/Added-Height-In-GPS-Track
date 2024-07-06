7c45a4f6d22e1ae4a704154a119a455aba961971
Vadimvr <r.vadim.90@gmail.com>
2024-07-06 11:34:52 +0300

    Добавьте файлы проекта.

---
+++ Added Height In GPS Track/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Added_Height_In_GPS_Track
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
