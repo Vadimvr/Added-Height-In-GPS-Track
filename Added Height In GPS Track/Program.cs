d23a7e1797e67aa5c4f842aa44d5ebe7dde8b4c6
Vadimvr <r.vadim.90@gmail.com>
2024-07-07 12:24:58 +0300

    Added adding heights from falls located in the project

--- Added Height In GPS Track/Program.cs
+++ Added Height In GPS Track/Program.cs
@@ -1,4 +1,6 @@
 ﻿using LibSRMTHeight;
+using System.Diagnostics;
+using System.Reflection.Metadata;
 using System.Xml;
 namespace Added_Height_In_GPS_Track
 {
@@ -6,23 +8,64 @@ namespace Added_Height_In_GPS_Track
     {
         public static void Main()
         {
-            XmlDocument doc = new XmlDocument();
-            doc.Load("тест2__20240706_1341.kml");
-            XmlNodeList elemList = doc.GetElementsByTagName("coordinates");
+            var allFiles = Directory.GetFiles(AppContext.BaseDirectory)
+                .Where(x => x.Length > 4
+                && x.Substring(x.Length - 4).Equals(".kml", StringComparison.InvariantCultureIgnoreCase) && !x.Contains("update"));
+            int i = 0;
+            foreach (var file in allFiles)
+            {
+                //var x = file.Substring(file.Length - 4) == ".kml";
+                Worker(file);
+            }
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"added {i}");
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+
+        private static void Worker(string path)
+        {
+            XmlDocument kml = new XmlDocument();
+            kml.Load(path);
+            XmlNode newNode = kml.CreateElement("name");
+            // oruxmaps track name
+            newNode.InnerXml = $"<![CDATA[{path.Substring(AppContext.BaseDirectory.Length)}]]>";
+
+
+            XmlNodeList doc = kml.GetElementsByTagName("Document");
+            if (doc != null && doc.Count == 1 && doc[0]!=null)
+            {
+                doc[0]?.AppendChild(newNode);
+            }
+            XmlNodeList elemList = kml.GetElementsByTagName("coordinates");
             if (elemList == null) return;
             for (int i = 0; i < elemList.Count; i++)
             {
                 if (elemList[i] == null || elemList[i].InnerXml == null) { continue; }
-
                 List<Coordinate> coordinates = CovertCoordinate.ConvertToDoubleCoordinates(elemList[i].InnerXml);
-
-                elemList[i].InnerXml = coordinates.Select(x => x.ToString()).Aggregate("", (current, next) => current + " " + next);
+                var res = Coordinate.AddPoint(coordinates);
+                elemList[i].InnerXml = res.Select(x => x.ToString()).Aggregate("", (current, next) => current + " " + next);
             }
-            doc.Save("тест2__20240706_1341.kml");
 
-            foreach (var item in Coordinate.GetError())
+            string newPath = path.Substring(0, path.Length - 4) + "-update" + path.Substring(path.Length - 4);
+            if (Coordinate.GetError().Count() == 0)
             {
-                Console.WriteLine(item);
+                kml.Save(newPath);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Added {newPath}");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"no height titles for\n\t{path}");
+                foreach (var item in Coordinate.GetError())
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"\tsrtm\\{item}");
+                    //Process.Start("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", "https://srtm.kurviger.de/SRTM3/Eurasia/" + item + ".zip");
+                }
+                Console.ResetColor();
             }
         }
     }
--- LibSRMTHeight/Coordinate.cs
+++ LibSRMTHeight/Coordinate.cs
@@ -1,9 +1,12 @@
-﻿using System.Globalization;
+﻿using System.Collections.Generic;
+using System.Globalization;
 
 namespace LibSRMTHeight
 {
     public class Coordinate
     {
+        public static double deltaY = 0.001;
+        public static double deltaX = 0.001;
         static Dictionary<string, int> filesIsNotExist = new Dictionary<string, int>();
         static Dictionary<string, byte[]> data = new Dictionary<string, byte[]>();
         static readonly int maxLengthArray = 1201 * 1201 * 2;
@@ -17,9 +20,10 @@ namespace LibSRMTHeight
         // высота
         public double Height { get; set; }
 
+        private static readonly string formatAfterDot = "0.0000000";
         public override string ToString()
         {
-            return $"{Longitude.ToString(nfi)},{Latitude.ToString(nfi)},{Height.ToString(nfi)}";
+            return $"{Longitude.ToString(formatAfterDot, nfi)},{Latitude.ToString(formatAfterDot, nfi)},{Height.ToString(nfi)}";
         }
         static NumberFormatInfo nfi = new NumberFormatInfo() { NumberDecimalSeparator = "." };
 
@@ -35,9 +39,9 @@ namespace LibSRMTHeight
             {
                 if (!File.Exists(srtmPath + key))
                 {
-                    if (!filesIsNotExist.ContainsKey(srtmPath + key))
+                    if (!filesIsNotExist.ContainsKey(key))
                     {
-                        filesIsNotExist.Add(srtmPath + key, 0);
+                        filesIsNotExist.Add(key, 0);
                     }
                     return;
                 }
@@ -52,23 +56,76 @@ namespace LibSRMTHeight
                 }
                 data.Add(key, value);
             }
-
-            int lat = (int)Math.Round((Latitude - (int)Latitude) * 1201);
-            int lon = (int)Math.Round((Longitude - (int)Longitude) * 1201);
-
+           
+            int lat = (int)((Latitude - (int)Latitude) * 1201);
+            int lon = (int)((Longitude - (int)Longitude) * 1201);
             int pos = ((1201 - lat - 1) * 1201 * 2) + lon * 2;
 
             if (pos < 0 || pos > maxLengthArray)
-                throw new ArgumentOutOfRangeException("Coordinates out of range.", "coordinates");
+            {
+                Console.WriteLine("{0} {1}",Latitude, Longitude, "Coordinates out of range.", "coordinates");
+                return;
+            }
 
             if ((value[pos] == 0x80) && (value[pos + 1] == 0x00))
-                Height = 0;
+            {
+                Console.WriteLine("(value[pos] == 0x80) && (value[pos + 1] == 0x00)");
+            }
             Height = (value[pos]) << 8 | value[pos + 1];
+            if(Height == 0)
+            {
+                Console.WriteLine(0);
+            }
         }
 
         public static IEnumerable<string> GetError()
         {
             return filesIsNotExist.Select(f => f.Key);
+        }
+
+        public static List<Coordinate> AddPoint(List<Coordinate> coordinates)
+        {
+            List<Coordinate> result = new List<Coordinate>();
+
+            for (int i = 0; i < coordinates.Count - 1; i++)
+            {
+                result.AddRange(AddPoint(coordinates[i], coordinates[i + 1]));
+            }
+            result.Add(coordinates[^1]);
+            return result;
+        }
+
+        private static List<Coordinate> AddPoint(Coordinate first, Coordinate second)
+        {
+            if (first.Latitude == 53.9730629533484)
+            {
+
+            }
+            var y = Math.Abs(first.Latitude - second.Latitude);
+            var x = Math.Abs(first.Longitude - second.Longitude);
+
+            List<Coordinate> result = new List<Coordinate>() { first };
+            if (x > deltaX || y > deltaY)
+            {
+                var mul = (int)(x / deltaX > y / deltaY ? x / deltaX : y / deltaY);
+
+                var mulY = first.Latitude > second.Latitude ? -1 : 1;
+                var mulX = first.Longitude > second.Longitude ? -1 : 1;
+
+                var sepY = y / mul * mulY;
+                var sepX = x / mul * mulX;
+                var last = first;
+
+                for (int i = 0; i < mul - 1; i++)
+                {
+                    var cor = new Coordinate() { Latitude = last.Latitude + sepY, Longitude = last.Longitude + sepX };
+                    cor.GetHeight();
+                    last = cor;
+                    result.Add(cor);
+                }
+            }
+
+            return result;
         }
     }
 }
